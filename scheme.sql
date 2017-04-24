69fc277bee1d198817940724103682c95cd0bf69
campovski <campovski@luna.campovski.eu>
2017-04-24 14:21:55 +0200

    Add database scheme.

---
+++ scheme.sql
@@ -0,0 +1,32 @@
+CREATE TABLE country (
+	id SERIAL PRIMARY KEY,
+	name TEXT
+);
+
+CREATE TABLE manufacturer (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	country INTEGER REFERENCES country
+);
+
+CREATE TABLE beer (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE,
+	review TEXT,
+	grade_taste CHAR(1),
+	grade_color CHAR(1),
+	grade_smell CHAR(1),
+	grade_smoothness CHAR(1),
+	grade_foam CHAR(1),
+	grade_total CHAR(1),
+	image TEXT,
+	manufacturer INTEGER REFERENCES review
+);
+
+CREATE TABLE burger (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	grade CHAR(1),
+	review TEXT,
+	manufacturer INTEGER REFERENCES manufacturer
+);
