1408f6b131898f2627c5cdccf53700070f803f39
Marcel Campovski <campovski@gmail.com>
2017-05-02 00:02:51 +0200

    Change deprecated files.

--- deprecated/db/country_parser.py
+++
@@ -1,5 +0,0 @@
-fout = open("countries.sql", "w")
-with open("tmp/countries.txt", "r") as fin:
-    for line in fin:
-        fout.write("INSERT INTO country (name) VALUES ('{0}');\n".format(line.strip()))
-fout.close()
